7339fe81e7f1473504309db00f5bd62ffb91fc56
Sofia Navarro <sofia.navarro@yahoo.dk>
2020-11-19 22:49:38 +0100

    model push

---
+++ model_data.R
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1fa3d135711573f20549d8ae8c33a24ea61dc01a52231fec911efb19c28189b1
+size 39300
---
+++ multinom_regression_feature.R
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4918c8faa2c625b283706a9594d90653a1faa30a82ebeb8d3038bae1dab89512
+size 3144
