362371d18416a088dec06c7d2e644fed79b82933
Sofia Navarro <sofia.navarro@yahoo.dk>
2020-12-15 19:31:06 +0100

    More explanations added

--- survey_analysis.R
+++ survey_analysis.R
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ac67a79f6a6e9696c35736cfc29b89d4ea15a8eb80d1d437bfb0c1d60ac890cf
-size 1635
+oid sha256:192865e476c6f0cd31326d733322db626692949ced242367e838a54bcfe36bb3
+size 1760
